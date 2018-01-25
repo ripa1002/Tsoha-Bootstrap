558516593f0c0a5a3f6a4f5d75661b72071c2923
Risto Sutinen <risto.sutinen@gmail.com>
2018-01-25 23:02:28 +0200

    Lisätty staattiset html-sivut

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,35 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        //View::make('home.html');
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        View::make('helloworld.html');
     }
-  }
+
+    public static function etusivu() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function ketjut() {
+        View::make('suunnitelmat/ketjut.html');
+    }
+
+    public static function kirjaudu() {
+        View::make('suunnitelmat/kirjaudu.html');
+    }
+    
+    public static function rekisteroidy() {
+        View::make('suunnitelmat/rekisteroidy.html');
+    }
+    
+    public static function muokkaaviestia() {
+        View::make('suunnitelmat/muokkaaviestia.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,26 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+$routes->get('/', function() {
+    HelloWorldController::etusivu();
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/aihe1', function() {
+    HelloWorldController::ketjut();
+});
+
+$routes->get('/kirjaudu', function() {
+  HelloWorldController::kirjaudu();
+});
+
+$routes->get('/rekisteroidy', function() {
+  HelloWorldController::rekisteroidy();
+});
+
+//tämän polku tulee vielä muuttumaan
+$routes->get('/muokkaa', function() {
+  HelloWorldController::muokkaaviestia();
+});
